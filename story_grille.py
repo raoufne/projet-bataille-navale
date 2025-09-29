b1bac98b379f5b4822b8ffe540337b210762b80e
Raouf Nechmi <152798689+raoufne@users.noreply.github.com>
2025-09-29 10:58:38 +0200

    Update story_grille.py

---
+++ story_grille.py
@@ -0,0 +1,19 @@
+from grille import Grille
+
+g = Grille(5, 8)
+print("Grille initiale :")
+print(g)
+try:
+    x = int(input("Entrez la ligne (0-based) : "))
+    y = int(input("Entrez la colonne (0-based) : "))
+except Exception:
+    print("Coordonnées invalides")
+    exit()
+
+if g.tirer(x, y):
+    print("Tir effectué")
+else:
+    print("Coordonnées hors grille")
+
+print("Grille après le tir :")
+print(g)
