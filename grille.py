feac5db2d6283b6aa64f4abd9fd7885d86ed9a47
Raouf Nechmi <152798689+raoufne@users.noreply.github.com>
2025-10-27 16:04:53 +0100

    Add internal grid list to Grille class
    Initialisation de l'attribut 'liste' dans la classe Grille pour représenter le plateau comme une liste simple de cases ('.'). Ajout des annotations de type pour les paramètres du constructeur.

--- grille.py
+++ grille.py
@@ -1,4 +1,5 @@
 class Grille:
-    def __init__(self, nb_lignes, nb_colonnes):
+    def __init__(self, nb_lignes: int, nb_colonnes: int):
         self.nb_lignes = nb_lignes
         self.nb_colonnes = nb_colonnes
+        self.liste = ['.'] * (nb_lignes * nb_colonnes)
