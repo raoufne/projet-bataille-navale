b89a9e2bc0d2552dd03c1cceaf977963d7a13016
Raouf Nechmi <152798689+raoufne@users.noreply.github.com>
2025-10-27 14:55:53 +0100

    Add initial project files and requirements
    Ajout des fichiers de structure (main.py, main_fonctionnalité.py, story_grille.py) et configuration initiale des dépendances (requirements.txt avec pytest).
    à faire : Coder la classe Grille avec son constructeur (__init__) et la méthode afficher().

---
+++ story_grille.py
@@ -0,0 +1,12 @@
+# from grille import Grille 
+
+
+# grille = Grille(5, 8)
+
+
+# while True:
+#     grille.afficher()
+#     x = int(input("Entrez la coordonnée X (colonne) : "))
+#     y = int(input("Entrez la coordonnée Y (ligne) : "))
+#     grille.tirer(x, y)
+
